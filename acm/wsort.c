b46b619659f16f076472840b7c480665260add08
wwssh <shaojwa@gmail.com>
2020-01-19 19:44:00 +0800

    Update wsort.c

--- acm/wsort.c
+++ acm/wsort.c
@@ -5,9 +5,10 @@ void swap(int *left, int *right) {
   int tmp = *left;
   *left = *right;
   *right = tmp;
-}
+} 
 
-void wsort(int *base, size_t first, size_t last) {
+// qsort
+void wqsorts(int *base, size_t first, size_t last) {
   if (first == last) {
     return;
   }
@@ -17,7 +18,7 @@ void wsort(int *base, size_t first, size_t last) {
     }
     return;
   }
-
+      
   size_t mid = (first + last) / 2;
   if (base[first] > base[last]) {
     swap(base + first, base + last);
@@ -42,9 +43,59 @@ void wsort(int *base, size_t first, size_t last) {
     }
   }
   swap(base + left, base + last - 1);
-  wsort(base, first, left - 1);
-  wsort(base, left + 1, last);
+  wqsorts(base, first, left - 1);
+  wqsorts(base, left + 1, last);
 }
+
+void wqsortl(int *base, size_t length) {
+  if (length == 1) {
+    return;
+  } else if (length == 2) {
+    if (base[0] > base[1]) { swap(base, base + 1); }
+    return;
+  }
+      
+  size_t mid = length / 2;
+  if (base[0] > base[length - 1]) {
+    swap(base, base + length - 1);
+  }
+  if (base[mid] > base[length - 1]) {
+    swap(base + mid, base + length - 1);
+  }
+  if (base[mid] < base[0]) {
+    swap(base + mid, base);
+  }
+  int pivot = base[mid];
+  swap(base + mid, base + length - 2);
+  size_t left = 0;
+  size_t right = length - 2;
+  for(;;) {
+    while (base[++left] < pivot){};
+    while (base[--right] > pivot){};
+    if (left < right) {
+      swap(base + left, base + right);
+    } else {
+      break;
+    }
+  }
+  swap(base + left, base + length - 2);
+  wqsortl(base, left);
+  wqsortl(base + left + 1,  length - left - 1);
+}
+
+
+// insert sort
+void wisort (int *base, size_t nmemb) {
+  int i = 0, j = 0;
+  for (i = 0; i < nmemb - 1; i++) {
+    for (j = i + 1; j < nmemb; j++) {
+      if (base[i] > base[j]){
+        swap(base + i, base + j);
+      }
+    }
+  }
+}
+
 
 int num[10000];
 int main() {
@@ -65,7 +116,9 @@ int main() {
     printf("before sort:\n");
     for (i = 0; i < count; i++) { printf("%d ", num[i]); }
     printf("\n");
-    wsort(num, 0, count - 1);
+    // wqsorts(num, 0, count - 1);
+    // wqsortl(num, count);
+    wisort(num, count); 
     printf("after sort:\n");
     for (i = 0; i < count; i++) { printf("%d ", num[i]); }
     printf("\n");
