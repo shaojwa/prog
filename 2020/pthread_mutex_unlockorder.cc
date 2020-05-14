6fcf26036069a8d34d94f342078af16d0ee3bfd9
wswsh <shaojwa@gmail.com>
2020-05-14 20:05:07 +0800

    Update pthread_mutex_unlockorder.cc

--- 2020/pthread_mutex_unlockorder.cc
+++ 2020/pthread_mutex_unlockorder.cc
@@ -5,13 +5,10 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <iostream>
-
 using namespace std;
-
 
 int threads_count = 10;
 pthread_mutex_t glock;
-
 
 int gettid() {
   return syscall(SYS_gettid);
@@ -24,13 +21,11 @@ int wait_to_lock() {
   pthread_mutex_unlock(&glock);
 }
 
-
 void* start_routine(void *param) {
   // cout << "i am #" << reinterpret_cast<int>(param) << " thread " << gettid() << endl;
   cout << "i am #" << (long long)param << " thread " << gettid() << endl;
   wait_to_lock();
 }
-
 
 void create_threads(pthread_t threads[], int threads_count) {
   for (int i = 0; i < threads_count; i++) {
@@ -50,11 +45,10 @@ void join_threads(pthread_t threads[], int threads_count) {
   }
 }
 
-
 int main(int argc, char *argv[]) {
-  pthread_t tids[threads_count];
-
+  pthread_t tids[threads_count];  
   pthread_mutex_init(&glock, NULL);
+  
   pthread_mutex_lock(&glock);
   cout << "create threads" << endl;
   create_threads(tids, threads_count);
@@ -62,5 +56,6 @@ int main(int argc, char *argv[]) {
   pthread_mutex_unlock(&glock);
   cout << "join threads" << endl;
   join_threads(tids, threads_count);
+  cout << "quit" << endl;
   return 0;
 }
