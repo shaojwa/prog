bc6674fd6807e55a5cc3aebdf96de4299de42b35
shaojwa <shaojwa@gmail.com>
2020-05-21 20:21:15 +0800

    Create 重载类中的比较运算符.cc

---
+++ std/重载类中的比较运算符.cc
@@ -0,0 +1,43 @@
+/*
+如果类作为set等元素的集合元素，那儿需要在类中重载比较运算符，重载的时候，operator< 必须是const修饰。
+*/
+
+#include <map>
+#include <set>
+#include <iostream>
+
+using namespace std;
+class Cls {
+public:
+    Cls(int n_ = 0): n(n_) {
+      cout << "Cls(n_)" << endl;
+    }
+    Cls &operator=(Cls &c) {
+      n = c.n;
+    }
+    bool operator<(const Cls &c) const {
+      cout << "operator<" << endl;
+      return n < c.n;
+    }
+    int n;
+};
+
+void test_func() {
+  set<Cls> cls_set;
+  Cls c(1), d(2);
+  cls_set.insert(c);
+  cls_set.insert(d);
+}
+
+int main(void) {
+  test_func();
+  return 0;
+}
+
+/* 输出
+Cls(n_)
+Cls(n_)
+operator<
+operator<
+operator<
+*/
