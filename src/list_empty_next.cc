4a09049b8dc08f128a4b9aec33ef5158d1076123
shaojwa <shaojwa@gmail.com>
2020-11-13 16:32:05 +0800

    update list_empty_next.cc

--- src/list_empty_next.cc
+++ src/list_empty_next.cc
@@ -6,17 +6,23 @@ using namespace std;
 int main()
 {
   list<int> l;
+  l.push_back(1);
+  l.push_back(5);
+  l.push_back(9);
   list<int>::iterator it = l.begin();
-  if (it == l.end()) {
-    cout << "yes" << endl;
-  } else {
-    cout << "no" << endl;
+  for (int i = 0; i < 10; i++) {
+    if (it == l.end()) {
+      cout << "it == l.end()" << endl;
+    } else {
+      cout << *it << endl;
+    }
+    ++it;
   }
-  ++it;
-  if (it == l.end()) {
-    cout << "yes" << endl;
-  } else {
-    cout << " no " << endl;
+  cout << "----" << endl; 
+  it = l.begin();
+  while (it != l.end()) {
+    cout << *it << endl;
+    it = std::next(it);
   }
   return 0;
 }
