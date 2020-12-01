2ab427149ecb8350845df646cdd9c5e8040c3d3e
shaojwa <shaojwa@gmail.com>
2020-12-01 14:14:54 +0800

    Update 1a.cc

--- acm/codeforces/1a.cc
+++ acm/codeforces/1a.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  int n, m, a;
-  cin >> n >> m >> a;
-  cout << (long long)((n+a-1)/a) * ((m+a-1)/a) << endl;
+    int n, m, a;
+    cin >> n >> m >> a;
+    cout << (long long)((n+a-1)/a) * ((m+a-1)/a) << endl;
 }
