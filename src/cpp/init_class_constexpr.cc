0e2e6f7734fc0f4d17681eb20671eacf4a94b413
shaojwa <shaojwa@gmail.com>
2020-12-06 12:31:59 +0000

    change indent

--- src/cpp/init_class_constexpr.cc
+++ src/cpp/init_class_constexpr.cc
@@ -3,18 +3,18 @@ using namespace std;
 
 class Dog {
 	public:
-    static constexpr bool m[2][2] {
-        {false, true}, {true, false}
-    };
-    int name;
+		static constexpr bool m[2][2] {
+			{false, true}, {true, false}
+		};
+		int name;
 };
 
 int main()
 {
-    Dog d;
-		for (int i = 0; i < sizeof(Dog::m); i++) {
-			int x = i / 2, y = i % 2;
-			cout << "a.matrix[" << i/2 << "][" << i%2 << "] = " << Dog::m[x][1] << endl;
-		}
-    return 0;
+	Dog d;
+	for (int i = 0; i < sizeof(Dog::m); i++) {
+		int x = i / 2, y = i % 2;
+		cout << "a.matrix[" << i/2 << "][" << i%2 << "] = " << Dog::m[x][1] << endl;
+	}
+	return 0;
 }
