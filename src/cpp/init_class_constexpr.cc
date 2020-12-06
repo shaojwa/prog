e4dc499fbcfad6f294a21bfccca01ee8a713f77c
shaojwa <shaojwa@gmail.com>
2020-12-06 12:22:57 +0000

    add init_class_constexpr.cc

--- src/cpp/init_class_constexpr.cc
+++ src/cpp/init_class_constexpr.cc
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class dog {
-    static constexpr bool matrix[][] {
+class Dog {
+	public:
+    static constexpr bool m[2][2] {
         {false, true}, {true, false}
-    }
+    };
     int name;
-}
+};
 
 int main()
 {
-    dog d;
-    cout << "a.matrix[0][0] " << d.matrix[0][0] << endl;
+    Dog d;
+		for (int i = 0; i < sizeof(Dog::m); i++) {
+			int x = i / 2, y = i % 2;
+			cout << "a.matrix[" << i/2 << "][" << i%2 << "] = " << Dog::m[x][1] << endl;
+		}
     return 0;
 }
