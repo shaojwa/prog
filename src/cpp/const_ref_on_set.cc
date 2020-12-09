aa954fca0a1bca34420c952dfd3256ac03cf1f86
wangshaojie <wang.shaojie@h3c.com>
2020-12-09 20:20:10 +0800

    the reference of set-element must be const

---
+++ src/cpp/const_ref_on_set.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <list>
+#include <set>
+#include <typeinfo>
+
+
+using namespace std;
+
+struct Dog {
+  Dog(int a): age(a) {
+    cout << "Dog(int a)" << endl;
+  }
+  int age;
+  operator int() const {
+    cout << "operator int() " <<  endl;
+    return age;
+  }
+};
+
+// #define TEST_LIST
+
+int main () {
+
+#ifdef TEST_LIST
+  list<Dog> lst;
+  lst.push_back(Dog(1));
+  lst.push_back(Dog(2));
+  lst.push_back(Dog(3));
+  for (list<Dog>::iterator it = lst.begin(); it != lst.end();  ++it) {
+    cout << it->age << endl;
+  }
+  for (Dog &it: lst) {
+    it.age = 4;
+  }
+  for (list<Dog>::iterator it = lst.begin(); it != lst.end();  ++it) {
+    cout << it->age << endl;
+  }
+#else
+  set<Dog> s;
+  s.insert(Dog(1));
+  s.insert(Dog(2));
+  s.insert(Dog(3));
+  for (set<Dog>::iterator it = s.begin(); it != s.end();  ++it) {
+    cout << it->age << endl;
+  }
+  for (const Dog &it: s) {
+    // it.age = 4;
+  }
+  for (set<Dog>::iterator it = s.begin(); it != s.end();  ++it) {
+    cout << it->age << endl;
+  }
+#endif
+  return 0;
+}
+
