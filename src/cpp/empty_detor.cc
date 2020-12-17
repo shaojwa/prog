154426ebcc81eddb03102c2bb0882a0bf9b6adc3
shaojwa <shaojwa@gmail.com>
2020-12-17 19:28:07 +0800

    Update empty_detor.cc

--- src/cpp/empty_detor.cc
+++ src/cpp/empty_detor.cc
@@ -1,20 +1,16 @@
 #include <iostream>
 using namespace std;
-
 struct A {
   private:
     ~A() {
       cout << "~A()" << endl;
     };
 };
-
 struct B : A {
 //  ~B() {
 //    cout << "~B()" << endl;
 //  };
 };
-
 int main() {
-  B b;
   return 0;
 } 
