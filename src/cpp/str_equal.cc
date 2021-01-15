1b1d1e684bfe3655584c95b202e3404ccce23ad2
shaojwa <shaojwa@gmail.com>
2021-01-15 21:42:33 +0800

    testing whether the string-compare is strict

---
+++ src/cpp/str_equal.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string str = "stat-hashxxx";
+  if (str == "stat-hash") {
+    cout << "yes" << endl;
+  } else {
+    cout << "no" << endl;
+  }
+  return 0;
+}
