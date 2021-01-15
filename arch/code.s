27577dc176857628866a7a4f3db1a19530472dd1
shaojwa <shaojwa@gmail.com>
2021-01-16 00:34:22 +0800

    Update code.s

--- arch/code.s
+++ arch/code.s
@@ -25,11 +25,10 @@ assemble:
 ```
 0x0000000000400740:    pushq  0x2008c2(%rip)        # 0x601008
 0x0000000000400746:    jmpq   *0x2008c4(%rip)       # 0x601010      
-0x0000000000400760 <+0>:     jmpq   *0x2008ba(%rip)    //  value of 0x601020 is 400766
-0x0000000000400766 <+6>:     pushq  $0x1               //
-0x000000000040076b <+11>:    jmpq   0x400740           //
-              
-              
+0x0000000000400760 <+0>:     jmpq   *0x2008ba(%rip)             //  value of 0x601020 is 400766
+0x0000000000400766 <+6>:     pushq  $0x1                        //
+0x000000000040076b <+11>:    jmpq   0x400740                    //
+--------            
 Dump of assembler code for function main():
   0x0000000000400959 <+0>:     push   %rbp                      // enter function, saving
   0x000000000040095a <+1>:     mov    %rsp,%rbp                 // rbp = rsp, the new frame
@@ -54,27 +53,7 @@ Dump of assembler code for function main():
    0x000000000040097e <+37>:    lea    -0x30(%rbp),%rax         // rax = 0xe410(d)
    0x0000000000400982 <+41>:    mov    %rdx,%rsi                // rsi = 0xe420
    0x0000000000400985 <+44>:    mov    %rax,%rdi                // rdi = 0xe410(d)
-   0x0000000000400988 <+47>:    callq  0x400a66 <dog_t::operator=(dog_t&&)>
-     (gdb) disass dog_t::operator=
-     Dump of assembler code for function dog_t::operator=(dog_t&&):
-     0x0000000000400a66 <+0>:     push   %rbp
-     0x0000000000400a67 <+1>:     mov    %rsp,%rbp
-     0x0000000000400a6a <+4>:     sub    $0x10,%rsp       //
-     0x0000000000400a6e <+8>:     mov    %rdi,-0x8(%rbp)  // put addr in stack
-     0x0000000000400a72 <+12>:    mov    %rsi,-0x10(%rbp) // put addr in stack
-     0x0000000000400a76 <+16>:    mov    -0x10(%rbp),%rdx // put value of address to rdx
-     0x0000000000400a7a <+20>:    mov    -0x8(%rbp),%rax  // put value of address to rax
-     0x0000000000400a7e <+24>:    mov    %rdx,%rsi        // 
-     0x0000000000400a81 <+27>:    mov    %rax,%rdi        //
-     0x0000000000400a84 <+30>:    callq  0x4007c0 <_ZNSsaSEOSs@plt>
-     0x0000000000400a89 <+35>:    mov    -0x10(%rbp),%rax
-     0x0000000000400a8d <+39>:    mov    0x8(%rax),%edx
-     0x0000000000400a90 <+42>:    mov    -0x8(%rbp),%rax
-     0x0000000000400a94 <+46>:    mov    %edx,0x8(%rax)
-     0x0000000000400a97 <+49>:    mov    -0x8(%rbp),%rax
-     0x0000000000400a9b <+53>:    leaveq
-     0x0000000000400a9c <+54>:    retq
-   
+   0x0000000000400988 <+47>:    callq  0x400a66 <dog_t::operator=(dog_t&&)>  
    0x000000000040098d <+52>:    lea    -0x20(%rbp),%rax     // rax = rbp-0x20
    0x0000000000400991 <+56>:    mov    %rax,%rdi            // 
    0x0000000000400994 <+59>:    callq  0x400a4c <dog_t::~dog_t()>
@@ -101,4 +80,24 @@ Dump of assembler code for function main():
    0x00000000004009de <+133>:   pop    %rbp
    0x00000000004009df <+134>:   retq
 End of assembler dump.
+--------
+(gdb) disass dog_t::operator=
+     Dump of assembler code for function dog_t::operator=(dog_t&&):
+     0x0000000000400a66 <+0>:     push   %rbp
+     0x0000000000400a67 <+1>:     mov    %rsp,%rbp
+     0x0000000000400a6a <+4>:     sub    $0x10,%rsp       //
+     0x0000000000400a6e <+8>:     mov    %rdi,-0x8(%rbp)  // put addr in stack
+     0x0000000000400a72 <+12>:    mov    %rsi,-0x10(%rbp) // put addr in stack
+     0x0000000000400a76 <+16>:    mov    -0x10(%rbp),%rdx // put value of address to rdx
+     0x0000000000400a7a <+20>:    mov    -0x8(%rbp),%rax  // put value of address to rax
+     0x0000000000400a7e <+24>:    mov    %rdx,%rsi        // 
+     0x0000000000400a81 <+27>:    mov    %rax,%rdi        //
+     0x0000000000400a84 <+30>:    callq  0x4007c0 <_ZNSsaSEOSs@plt>
+     0x0000000000400a89 <+35>:    mov    -0x10(%rbp),%rax
+     0x0000000000400a8d <+39>:    mov    0x8(%rax),%edx
+     0x0000000000400a90 <+42>:    mov    -0x8(%rbp),%rax
+     0x0000000000400a94 <+46>:    mov    %edx,0x8(%rax)
+     0x0000000000400a97 <+49>:    mov    -0x8(%rbp),%rax
+     0x0000000000400a9b <+53>:    leaveq
+     0x0000000000400a9c <+54>:    retq
 ```
